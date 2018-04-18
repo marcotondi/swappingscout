759385c1a098fb6ffab317cbdf51e5b6d51a7157
Marco Tondi <marcotondi@gmail.com>
2018-04-18 14:45:05 +0200

    aggiunta la classe active sulla navbar

--- client/js/pages/navigation/navigationController.js
+++ client/js/pages/navigation/navigationController.js
@@ -3,14 +3,16 @@
 
 	var app = angular.module('swappingscout');
 
-	app.controller('navigationController', ['$rootScope',
-		'$scope',
-		'$route', navigationController]);
+	app.controller('navigationController', ['$scope', '$location', navigationController]);
 
-	function navigationController($rootScope, $scope, $route) {
+	function navigationController($scope, $location) {
 
 		$scope.header = {
 			version: '1.0.0 beta'
+		};
+
+		$scope.isActive = function (viewLocation) {
+			return viewLocation === $location.path();
 		};
 
 	}
