d025b3e27a162306d7f7fd432a8d94083604543d
Marco Tondi <marcotondi@gmail.com>
2018-04-19 16:57:56 +0200

    modifiche interfaccia

--- client/js/pages/home/homeController.js
+++ client/js/pages/home/homeController.js
@@ -3,8 +3,23 @@
 
 	var app = angular.module('swappingscout');
 
-	app.controller('homeController', ['$scope', '$http', '$timeout', 'uiGridConstants', 'growl', homeController]);
+	app.controller('homeController', ['$scope', '$timeout', 'growl', homeController]);
 
-	function homeController($scope, $http, $timeout, uiGridConstants, growl) {
+	function homeController($scope, $timeout, growl) {
+		var smiles = [';D', '=P', 'O_O', 'O.o', ':)', '#.#', 'XD', '-_-', '^_^'];
+
+		$scope.smile = '';
+
+		$scope.giveAsmile = function () {
+			return smiles[getRandom(0, smiles.length - 1)].toString();
+		};
+
+		function getRandom(min, max) {
+			return parseInt(Math.random() * (max - min) + min);
+		};
+
+		$timeout(function () {
+			$scope.smile = $scope.giveAsmile();
+		})
 	}
 })();
